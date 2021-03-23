b0a8f5f2eae5074c8c136e68d8ab15d776bb5cbe
dcartertod <david@carter-tod.com>
2021-03-23 19:55:30 -0400

    0.0.1 first version

---
+++ ZoteroAbstractCleaner/chrome/content/include.js
@@ -0,0 +1,8 @@
+
+
+Zotero.debug("ZoteroAbstractCleaner loading",3);
+
+// I have no idea what this does - copied from someone else
+
+Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader).loadSubScript("chrome://ZoteroAbstractCleaner/content/ZoteroAbstractCleaner.js");
+
